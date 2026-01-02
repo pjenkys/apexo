e18ff0ae9cbe66f9658aa1d30886e3932d060916
elselawi <alielselawi@gmail.com>
2026-01-02 23:12:44 +0300

    ability to set size in acrylicButton

--- lib/common_widgets/acrylic_button.dart
+++ lib/common_widgets/acrylic_button.dart
@@ -7,9 +7,17 @@ class AcrylicButton extends StatelessWidget {
   final VoidCallback? onPressed;
   final double elevation;
   final Gradient? gradient;
+  final double size;
 
-  const AcrylicButton(
-      {super.key, required this.icon, required this.text, required this.onPressed, this.gradient, this.elevation = 1});
+  const AcrylicButton({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.onPressed,
+    this.gradient,
+    this.elevation = 1,
+    this.size = 14,
+  });
   @override
   Widget build(BuildContext context) {
     return Acrylic(
@@ -24,7 +32,21 @@ class AcrylicButton extends StatelessWidget {
           focusable: true,
           autofocus: true,
           icon: Row(
-            children: [Icon(icon), const SizedBox(width: 5), Txt(text)],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                size: size,
+                color: FluentTheme.of(context).inactiveColor,
+              ),
+              if (text.isNotEmpty) ...[
+                const SizedBox(width: 5),
+                Txt(
+                  text,
+                  style: TextStyle(fontSize: size),
+                )
+              ]
+            ],
           ),
           onPressed: onPressed,
         ),
