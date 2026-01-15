f7967a16d00326a635e83e05d822cbfee13c8382
elselawi <alielselawi@gmail.com>
2026-01-15 20:42:05 +0300

    confirm delete background

--- lib/common_widgets/confirm_delete_flyout.dart
+++ lib/common_widgets/confirm_delete_flyout.dart
@@ -2,52 +2,66 @@ import 'package:apexo/services/localization/locale.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 
 class ConfirmDeleteFlyout extends StatelessWidget {
-  const ConfirmDeleteFlyout(
-      {super.key, required this.onConfirm, required this.controller});
+  const ConfirmDeleteFlyout({
+    super.key,
+    required this.onConfirm,
+    required this.controller,
+  });
 
   final VoidCallback onConfirm;
   final FlyoutController controller;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Txt("${txt("delete")}?"),
-        const SizedBox(width: 10),
-        FilledButton(
-          onPressed: () {
-            controller.close();
-            onConfirm();
-          },
-          style: const ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Colors.errorPrimaryColor),
-            foregroundColor: WidgetStatePropertyAll(Colors.white),
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Txt("${txt("delete")}?"),
+          const SizedBox(width: 10),
+          FilledButton(
+            onPressed: () {
+              controller.close();
+              onConfirm();
+            },
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.errorPrimaryColor),
+              foregroundColor: WidgetStatePropertyAll(Colors.white),
+            ),
+            child: Row(
+              children: [
+                const Icon(FluentIcons.delete),
+                const SizedBox(width: 5),
+                Txt(txt("delete")),
+              ],
+            ),
           ),
-          child: Row(
-            children: [
-              const Icon(FluentIcons.delete),
-              const SizedBox(width: 5),
-              Txt(txt("delete")),
-            ],
-          ),
-        ),
-        const SizedBox(width: 10),
-        FilledButton(
-          onPressed: () => controller.close(),
-          style: const ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Colors.grey),
-            foregroundColor: WidgetStatePropertyAll(Colors.white),
-          ),
-          child: Row(
-            children: [
-              const Icon(FluentIcons.cancel),
-              const SizedBox(width: 5),
-              Txt(txt("cancel")),
-            ],
-          ),
-        )
-      ],
+          const SizedBox(width: 10),
+          FilledButton(
+            onPressed: () {
+              if (controller.isOpen) {
+                controller.close();
+              }
+            },
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.grey),
+              foregroundColor: WidgetStatePropertyAll(Colors.white),
+            ),
+            child: Row(
+              children: [
+                const Icon(FluentIcons.cancel),
+                const SizedBox(width: 5),
+                Txt(txt("cancel")),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
